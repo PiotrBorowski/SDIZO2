eb1b1c28450d9dca8a99b77a5d9cc562267badb4
LAPTOP-SLRMPIQU\Piotr Borowski <piotrborowski97@op.pl>
2018-05-04 23:32:12 +0200

    implementacja listy sasiedztwa

--- SDIZO2/AdjacencyListGraph.cpp
+++ SDIZO2/AdjacencyListGraph.cpp
@@ -1,12 +1,65 @@
 #include "stdafx.h"
 #include "AdjacencyListGraph.h"
+#include <ostream>
+#include <iostream>
+#include <fstream>
 
 
 AdjacencyListGraph::AdjacencyListGraph()
 {
+	vertices = 15;
+	edges = 15;
+	directed = false;
+	TableOfLists = new std::list<Edge>[vertices];
+}
+
+AdjacencyListGraph::AdjacencyListGraph(std::string filename, bool directed)
+{
+	this->directed = directed;
+
+	std::ifstream file;
+	file.open(filename);
+
+	file >> edges;
+	file >> vertices;
+
+	TableOfLists = new std::list<Edge>[vertices];
+
+	uint source, dest, weight;
+	while (file >> source >> dest >> weight)
+	{
+		AddEdge(source, dest, weight);
+	}
 }
 
 
 AdjacencyListGraph::~AdjacencyListGraph()
 {
+	delete[] TableOfLists;
 }
+
+void AdjacencyListGraph::AddEdge(uint source, uint dest, uint weight)
+{
+	Edge edge;
+	edge.weight = weight;
+	edge.vertex = dest;
+
+	TableOfLists[source].push_back(edge);
+}
+
+void AdjacencyListGraph::Print()
+{
+	std::cout << std::endl;
+
+	for (int i = 0; i < vertices; ++i)
+	{
+		std::cout << i << ": ";
+		for (Edge element : TableOfLists[i])
+		{
+			std::cout << element.vertex << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+
--- SDIZO2/AdjacencyListGraph.h
+++ SDIZO2/AdjacencyListGraph.h
@@ -1,8 +1,26 @@
 #pragma once
+#include <list>
+
+typedef unsigned int uint;
+
+struct Edge
+{
+	uint vertex;
+	uint weight;
+};
+
 class AdjacencyListGraph
 {
 public:
 	AdjacencyListGraph();
+	AdjacencyListGraph(std::string filename, bool directed);
 	~AdjacencyListGraph();
+	void AddEdge(uint source, uint dest, uint weight);
+	void Print();
+private:
+	std::list<Edge> *TableOfLists;
+	uint vertices, edges;
+	bool directed;
+	int* weights;
 };
 
